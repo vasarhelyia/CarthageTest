770f7e13dcfaf5fb9ab6aad33388ef64aec48a8a
vasarhelyia <vasarhelyi.agi@gmail.com>
2016-03-04 21:02:38 +0100

    Add Cartfiles

---
+++ CarthageTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CarthageTest
+//
+//  Created by Vásárhelyi Ágnes on 04/03/16.
+//  Copyright © 2016 Vásárhelyi Ágnes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
